2abc29e096654f416436489ddec8b525153af6a8
codemercl <developer.venium@gmail.com>
2023-11-03 16:34:17 +0200

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/logo',
-      },
-    },
-  },
-  publicPath: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
